94254ff4bd1ac039925643665484339f2856b758
Ganbayar Gansukh <ganbayar.gansukh@kellpro.com>
2017-11-06 14:01:07 -0600

    Initial Commit

---
+++ EggTimer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EggTimer
+//
+//  Created by Ganbayar Gansukh on 11/6/17.
+//  Copyright © 2017 Ganbayar Gansukh. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Ganbayar Gansukh on 11/6/17.
+//  Copyright © 2017 Ganbayar Gansukh. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
